a62c12a7fa420e8e502673dd7e11f1847fe239be
Beku001 <nurgazevbekbolsun906@gmail.com>
2023-07-27 19:43:48 +0600

    frontcur

---
+++ src/components/Navigation/OurCurces/Vupusk/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./index.scss"
+
+const Vupusk = () => {
+  return (
+    <div id="vupusk">
+      <div className="container">
+        <div className="vupusk">
+          <div className="vupusk--block">
+            <div className="vupusk--block__group">
+            <p>2000+</p>
+            <span>Выпусников</span>
+            </div>
+          <div className="vupusk--block__group">
+            <p>2</p>
+            <span>Года</span>
+          </div>
+          <div className="vupusk--block__group">
+            <p>10+</p>
+            <span>Менторов</span>
+          </div>
+           <div className="vupusk--block__group">
+            <p>95%+</p>
+            <span>Трудоустроились</span>
+          </div>
+          </div>
+        </div>
+     
+      </div>
+    </div>
+  );
+};
+
+export default Vupusk;
---
+++ src/components/Navigation/OurCurces/otzuv/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './index.scss'
+import {HiArrowLongLeft,HiArrowLongRight} from 'react-icons/hi2'
+import ot from "../../../../img/ot.png"
+import Slider from "react-slick";
+
+const Otzyv = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      };
+  return (
+       <div className="otzyv">
+        <div className="otzyv--text">
+            <h3>Отзывы студентов</h3>
+            <div className="otzyv--text__icon">
+            <HiArrowLongLeft className='icon'/>
+          <HiArrowLongRight className='icon'/>
+            </div>
+        </div>
+       <div className="otzyv--slid">
+       <Slider {...settings} className='otzyv--slid__group'>
+            <div className="otzyv--slid__block">
+                <img src={ot} alt="img" />
+            </div> 
+            <div className="otzyv--slid__block">
+                <img src={ot} alt="img" />
+            </div>
+            <div className="otzyv--slid__block">
+                <img src={ot} alt="img" />
+            </div> 
+        </Slider>
+       </div>
+      </div>
+
+  )
+}
+
+export default Otzyv
