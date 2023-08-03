b822b76956f59de415e7bd4f8423061c1a049ea7
Asad <130597420+Asad220520@users.noreply.github.com>
2023-08-03 22:44:24 +0600

    head (#12)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,22 +3,23 @@ import { Link, NavLink, useNavigate, useParams } from "react-router-dom";
 import "./index.scss";
 import { GrLanguage } from "react-icons/gr";
 import logo from "../../img/logo.png";
+
 const Header = () => {
   const handleContactClick = (event) => {
     event.preventDefault();
     const footer = document.getElementById("contact");
     footer.scrollIntoView({ behavior: "smooth" });
   };
-  const [modal, setModal] = useState(false);
+
   const nav = useNavigate();
-  const [modal,setModal] = useState(false)
-  const [colo,setColo] = useState(1)
-  const {id} = useParams()
-  const nav = useNavigate()
+  const [modal, setModal] = useState(false);
+  const [colo, setColo] = useState(1);
+  const { id } = useParams();
+
   return (
     <div id="header">
       <div className="container">
-        <div style={{background:'--hover-color:'  }} className="header">
+        <div style={{ background: "--hover-color:" }} className="header">
           <nav className="nav">
             <Link to={"/"}>
               <img src={logo} alt="" />
@@ -56,77 +57,59 @@ const Header = () => {
                   }}
                 >
                   <li
-                    className="li"
-                    style={{
-                      borderBottom: "1px solid black",
-                      transition: ".4s",
-                      cursor: "pointer",
-                      fontSize: "18px",
-                      color: "black",
-                      textTransform: "capitalize",
+                    onClick={() => {
+                      setColo(1);
+                      nav("/disain");
+                      setModal(false);
                     }}
-                  >
-                    fronEnd
-                  </li>
-                  <li
                     className="li"
                     style={{
+                      color: colo === 1 ? "blue" : "black",
                       borderBottom: "1px solid black",
                       transition: ".4s",
                       cursor: "pointer",
                       fontSize: "18px",
-                      color: "black",
                       textTransform: "capitalize",
                     }}
                   >
                     disain
                   </li>
                   <li
+                    onClick={() => {
+                      nav("/frontt");
+                      setColo(2);
+                      setModal(false);
+                    }}
                     className="li"
                     style={{
+                      color: colo === 2 ? "blue" : "black",
                       borderBottom: "1px solid black",
                       transition: ".4s",
                       cursor: "pointer",
                       fontSize: "18px",
-                      color: "black",
+                      textTransform: "capitalize",
+                    }}
+                  >
+                    fronEnd
+                  </li>
+                  <li
+                    onClick={() => {
+                      nav("/bekend");
+                      setColo(3);
+                      setModal(false);
+                    }}
+                    className="li"
+                    style={{
+                      color: colo === 3 ? "blue" : "black",
+                      borderBottom: "1px solid black",
+                      transition: ".4s",
+                      cursor: "pointer",
+                      fontSize: "18px",
                       textTransform: "capitalize",
                     }}
                   >
                     bakend
                   </li>
-              <li style={{position:'relative'}} className="menu__item">
-                <NavLink onClick={() =>{
-                  // setColo(1)
-                   setModal(!modal)
-                }} to={"disain"} className="menu__link">
-                  О курсах
-                </NavLink>
-                <ul style={{
-                  boxShadow:'8px 8px 10px #000000ad',
-                  background:'white',
-                  padding:'10px 35px 15px 35px ',top:'30px',left:'-15px',
-                  borderRadius:'15px',
-                  position:'absolute',
-                  zIndex:'3',
-                  display: modal ? 'block' : 'none'}}>
-                  <li onClick={() => {
-                    setColo(1)
-                    nav("/disain")
-                    setModal(false)
-                  }} className="li" style={{color: colo === 1 ? 'blue' : 'black', 
-                    borderBottom:'1px solid black',transition:'.4s',cursor:'pointer'  ,fontSize:'18px',textTransform:'capitalize'}}>disain</li>
-                  <li onClick={() => {
-                    nav("/frontt")
-                    setColo(2)
-                    setModal(false)
-                  }} className="li" style={{color: colo === 2 ? 'blue' : 'black',
-                    borderBottom:'1px solid black',transition:'.4s',cursor:'pointer'  ,fontSize:'18px',textTransform:'capitalize'}}>fronEnd</li>
-                  <li onClick={() => {
-                    nav("/bekend")
-                    setColo(3)
-                    setModal(false)
-                  }} className="li" style={{color: colo === 3 ? 'blue' : 'black',
-                    borderBottom:'1px solid black',transition:'.4s',cursor:'pointer'  ,fontSize:'18px',textTransform:'capitalize'}}>bakend</li>
                 </ul>
               </li>
               <li className="menu__item">
