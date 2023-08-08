849f7af53c11f62c9a29f526c4d886a61981fa1d
Beku001 <126578152+nurgazievbekbolsun@users.noreply.github.com>
2023-08-08 13:03:35 +0600

    add (#20)
    Co-authored-by: Beku001 <nurgazevbekbolsun906@gmail.com>

--- src/components/Navigation/OurCurces/Kursy/index.js
+++ src/components/Navigation/OurCurces/Kursy/index.js
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 import "./index.scss";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { useEffect } from "react";
-
-const Kursy = ({ el }) => {
-  const { id } = useParams();
-  const [detail, setDetail] = useState([]);
-  useEffect(() => {
-    axios(`http://13.48.46.219/api/v1/courses/courses/`).then((res) =>
-      setDetail(res.data.results)
-    );
-  }, []);
+// import { useEffect } from "react";
+import front from "../../../../img/front.png"
+const Kursy = () => {
+  // const { id } = useParams();
+  // const [detail, setDetail] = useState([]);
+  // useEffect(() => {
+  //   axios(`http://3.83.165.209/api/v1/courses/courses/`).then((res) =>
+  //     setDetail(res.data.results)
+  //   );
+  // }, []);
   // console.log('detail',detail);
   const nav = useNavigate();
   return (
@@ -19,22 +19,36 @@ const Kursy = ({ el }) => {
       <div className="izyk">
         <div className="izyk--front">
           <div className="izyk--front__block">
-            <img
+            {/* <img
               className="izyk--front__block--img"
               src={el.images.map((el) => el.image)}
               alt="img"
-            />
+            /> */}
+            <img src={front} alt="img" />
           </div>
           <div className="izyk--front__group">
-            <h2>{el.name}</h2>
-            <p>{el.description}</p>
+            <h2>hj;k'
+              {/* {el.name} */}
+            </h2>
+            <p>lhojpk[l[]]
+              {/* {el.description} */}
+            </p>
             <div className="izyk--front__group--btns">
-              {el.courses_stacks.map((el) => (
+              {/* {el.courses_stacks.map((el) => (
                 <button>{el.stack}</button>
-              ))}
+              ))} */}
+              <button>w</button>
+              <button>ed</button>
+              <button>d</button>
+              <button>df</button>
+              <button>df</button>
+              <button>df</button>
+              <button>df</button>
             </div>
             <div className="izyk--front__group--btn">
-              <button onClick={() => nav(`/front/${el.id}`)} className="btn1">
+              <button 
+              // onClick={() => nav(`/front/${el.id}`)}
+               className="btn1">
                 Подробнее
               </button>
               <button className="btn2">Оставить заявку</button>
