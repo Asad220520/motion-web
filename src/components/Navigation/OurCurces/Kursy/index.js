c1c7d81542b40bd9dc01dc48219961b3ca13a21c
Beku001 <nurgazevbekbolsun906@gmail.com>
2023-08-11 12:03:55 +0600

    perdel

--- src/components/Navigation/OurCurces/Kursy/index.js
+++ src/components/Navigation/OurCurces/Kursy/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.scss";
+import "./media.scss"
 import { useNavigate } from "react-router-dom";
 
 const Kursy = ({ el }) => {
--- src/components/Navigation/OurCurces/begushiStrak/index.js
+++ src/components/Navigation/OurCurces/begushiStrak/index.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 import "./index.scss";
+import "./media.scss"
 import beg1 from "../../../../img/бегу.png";
 import beg2 from "../../../../img/бегу 2.png";
 
 const BegushiStrak = () => {
   return (
     <div id="begushi">
-      <div className="">
         <marquee className="begu">
-        <img src={beg1} alt="img" /> 
+        <img className="begu--img" src={beg1} alt="img" /> 
         </marquee> 
          <marquee className="begu2" behavior="" direction="">
-          <img src={beg2} alt="img" className="img2" />
+          <img className="begu2--img" src={beg2} alt="img"  />
         </marquee>
-      </div>
     </div>
   );
 };
