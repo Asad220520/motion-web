d2f60ac6058333f97e484356703447f4b093d538
Asad <asadkhakimov05@gmail.com>
2023-08-24 05:14:42 +0600

    f

--- src/components/Navigation/OurCurces/begushiStrak/index.js
+++ src/components/Navigation/OurCurces/begushiStrak/index.js
@@ -16,3 +16,25 @@ const BegushiStrak = () => {
 };
 
 export default BegushiStrak;
+
+
+// import React, { useState } from "react";
+// import "./index.scss";
+// import "./media.scss"
+// import beg1 from "../../../../img/beg.png";
+// import beg2 from "../../../../img/бегу 2.png";
+
+// const BegushiStrak = () => {
+//   return (
+//     <div id="begushi">
+//         <marquee className="begu">
+//         <img className="begu--img" src={beg1} alt="img" /> 
+//         </marquee> 
+//          <marquee className="begu2" behavior="" direction="">
+//           <img className="begu2--img" src={beg2} alt="img"  />
+//         </marquee>
+//     </div>
+//   );
+// };
+
+// export default BegushiStrak;
